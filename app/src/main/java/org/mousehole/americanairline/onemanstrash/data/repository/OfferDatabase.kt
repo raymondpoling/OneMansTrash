46dae4368a7d2c97a1590f704eedbd21bbb49c30
Raymond Poling <ruguerv@gmail.com>
2021-02-10 02:20:20 -0500

    Fixed watches

--- app/src/main/java/org/mousehole/americanairline/onemanstrash/data/repository/OfferDatabase.kt
+++ app/src/main/java/org/mousehole/americanairline/onemanstrash/data/repository/OfferDatabase.kt
@@ -12,6 +12,7 @@ import com.google.firebase.ktx.Firebase
 import org.mousehole.americanairline.onemanstrash.model.Offer
 import org.mousehole.americanairline.onemanstrash.utils.Constants.BIDON
 import org.mousehole.americanairline.onemanstrash.utils.Constants.OFFERS
+import org.mousehole.americanairline.onemanstrash.utils.Constants.USERS
 import org.mousehole.americanairline.onemanstrash.utils.Constants.WATCHERS
 import org.mousehole.americanairline.onemanstrash.utils.DebugLogger.debug
 
@@ -29,7 +30,7 @@ object OfferDatabase {
         offer.id = id.toString()
         id.setValue(offer)
         val addId = Firebase.database
-                .getReference(OfferDatabase.makePath(offer.seller, OFFERS)).push()
+                .getReference(makePath(USERS, offer.seller, OFFERS)).push()
         addId.setValue(id.toString())
     }
 
@@ -60,8 +61,8 @@ object OfferDatabase {
     private val watchData = MutableLiveData<List<Offer>>()
 
     fun getWatchData() : LiveData<List<Offer>> {
-        val user = Firebase.auth.currentUser?.uid?:""
-        val orderRef = Firebase.database.getReference(makePath(user, WATCHERS))
+        debug("uid is? ${getUid()}")
+        val orderRef = Firebase.database.getReference(OFFERS).orderByChild(makePath(WATCHERS, getUid())).equalTo(true)
         orderRef.addValueEventListener(UpdateValueEventListener(watchData))
         return watchData
     }
@@ -69,14 +70,14 @@ object OfferDatabase {
     fun setWatch(offer:Offer) {
         val user = Firebase.auth.currentUser?.uid?:""
         debug("offer id is: ${offer.id}")
-        Firebase.database.getReference(makePath(user, WATCHERS, Uri.parse(offer.id).lastPathSegment.toString())).setValue(offer)
+        Firebase.database.getReference(makePath(USERS, user, WATCHERS, Uri.parse(offer.id).lastPathSegment.toString())).setValue(offer)
         Firebase.database.getReference(makePath(OFFERS, Uri.parse(offer.id).lastPathSegment.toString(), WATCHERS, user)).setValue(true)
     }
 
     fun deleteWatch(offer:Offer) {
         val user = Firebase.auth.currentUser?.uid?:""
         debug("offer id is: ${offer.id}")
-        Firebase.database.getReference(makePath(user, WATCHERS, Uri.parse(offer.id).lastPathSegment.toString())).removeValue()
+        Firebase.database.getReference(makePath(USERS, user, WATCHERS, Uri.parse(offer.id).lastPathSegment.toString())).removeValue()
         Firebase.database.getReference(makePath(OFFERS, Uri.parse(offer.id).lastPathSegment.toString(), WATCHERS, user)).removeValue()
     }
 
